87464dbfb3e79bd85a041a946d3f20a304dd6e28
baolam <lam01662052827@gmail.com>
2024-07-02 08:23:24 +0700

    Updating

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const express = require("express");
+const ip = require("ip");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use("/", express.static(__dirname + "/layout/build"));
+app.get("/", (_req, res) => {
+  res.sendFile(__dirname + "/layout/build/index.html");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Dịch vụ đã được khởi chạy tại cổng: ${PORT}`);
+  console.log(
+    `Kết nối thông qua địa chỉ http://localhost:${PORT} hoặc http://${ip.address()}:${PORT}`
+  );
+});
